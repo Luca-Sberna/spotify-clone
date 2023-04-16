3c411c624f60c07fa1eda4c67fb66d2163fb7775
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-04-16 23:59:21 +0200

    adj-favourite-home

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { setPlay } from "../redux/reducers/playerReducer";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Col, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Hearth from "../assets/imgs/icons/hearth.gif";
 import { setFavouriteSong } from "../redux/reducers/favouriteSong";
+import HearthBroken from "../assets/imgs/icons/hearth-broken.png";
 
 const HomeContent = ({ data }) => {
   const dispatch = useDispatch();
+  const favouriteSong = useSelector((state) => state.favourite);
+
   const handleClick = (el) => {
     dispatch(setPlay(el));
   };
@@ -18,33 +21,46 @@ const HomeContent = ({ data }) => {
 
   return (
     <>
-      {data.slice(0, 6).map((el, i) => {
+      {data.slice(0, 6).map((track) => {
         return (
-          <Container key={el.id}>
+          <Container key={track.id}>
             <Col className="col text-center ">
               <Container className="position-relative overflow-hidden ">
                 <img
                   className="img-fluid "
-                  src={el.album.cover_medium}
-                  alt={el.title}
+                  src={track.album.cover_medium}
+                  alt={track.title}
                   onClick={() => {
-                    handleClick(el);
+                    handleClick(track);
                   }}
                 />
-                <img
-                  src={Hearth}
-                  alt="..."
-                  className="favourite-btn position-absolute w-25"
-                  onClick={() => {
-                    handleClickFavourite(el);
-                  }}
-                />
+                {!favouriteSong.includes(track.id) ? (
+                  <img
+                    src={Hearth}
+                    alt="..."
+                    className="favourite-btn position-absolute w-25"
+                    onClick={() => {
+                      handleClickFavourite(track.id);
+                    }}
+                  />
+                ) : (
+                  <img
+                    src={HearthBroken}
+                    alt="..."
+                    className="favourite-btn position-absolute w-25"
+                    onClick={() => {
+                      handleClickFavourite(track.id);
+                    }}
+                  />
+                )}
               </Container>
               <Container className="d-flex flex-column pt-1 pb-4">
-                <Link className="text-truncate" to={`/album/${el.album.id}`}>
-                  {el.album.title}
+                <Link className="text-truncate" to={`/album/${track.album.id}`}>
+                  {track.album.title}
                 </Link>
-                <Link to={`/artist/${el.artist.id}`}>{el.artist.name}</Link>
+                <Link to={`/artist/${track.artist.id}`}>
+                  {track.artist.name}
+                </Link>
               </Container>
             </Col>
           </Container>
